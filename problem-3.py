1b91ed67317bf3ed0d6a4b8dcd4c83ba10342815
rparpail <remi.parpaillon@gmail.com>
2014-11-30 15:42:45 +0100

    Resolve problem 3 : Get the largest prime divisor
    This method is kind of brutal : check the modulo of the number with all prime number lower than its half.

---
+++ problem-3.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+# "THE BEER-WARE LICENSE":
+# <remi.parpaillon@gmail.com> wrote this file. As long as you retain this notice you
+# can do whatever you want with this stuff. If we meet some day, and you think
+# this stuff is worth it, you can buy me a beer in return. Remi Parpaillon
+
+
+def prime_numbers(limit):
+    number = 3
+    primes = [2, number]
+    is_number_prime = True
+
+    yield 2
+    while number < limit:
+        if is_number_prime:
+            primes.append(number)
+            yield number
+            number += 2
+        else:
+            number += 2
+
+        is_number_prime = True
+
+        for a in primes:
+            if number % a == 0:
+                is_number_prime = False
+                break
+
+
+def get_prime_divisor(number):
+    for i in prime_numbers(number / 2):
+        if number % i == 0:
+            print('Prime divisor : %d' % i)
+
+get_prime_divisor(int(input('For which number do you want to find its greatest prime divisor ? : ')))
