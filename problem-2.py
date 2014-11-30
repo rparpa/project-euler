97638e6c3165a14460c7b2cd4e08d0cbb657a6d5
rparpail <remi.parpaillon@gmail.com>
2014-11-30 13:10:26 +0100

    Fix some typo on the license

--- problem-2.py
+++ problem-2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 # "THE BEER-WARE LICENSE":
-# <remi.parpaillon@gmail.com> wrote this file.  As long as you retain this notice you
+# <remi.parpaillon@gmail.com> wrote this file. As long as you retain this notice you
 # can do whatever you want with this stuff. If we meet some day, and you think
 # this stuff is worth it, you can buy me a beer in return. Remi Parpaillon
 
